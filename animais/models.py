3cd09d70741e95916dcf8b6d4e4ef8bf7d91f349
joaosrios <joao.srios07@gmail.com>
2022-07-05 11:24:26 -0300

    mudança no limite de caracteres no nome do animal

--- animais/models.py
+++ animais/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Animal(models.Model):
-    nome_animal = models.CharField(max_length=20)
+    nome_animal = models.CharField(max_length=25)
     predador = models.CharField(max_length=5)
     venenoso = models.CharField(max_length=5)
     domestico = models.CharField(max_length=5)
